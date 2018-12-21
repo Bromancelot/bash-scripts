618c0c39b336205593daf1eaf49749e99f29be6b
Michael A. Ferero <mikfer@localhost.localdomain>
2018-12-21 12:54:48 -0500

    added code to inplement join two lines

--- startup.lua
+++ startup.lua
@@ -7,7 +7,7 @@ function CurrentLine ()
 end
 
 function EditWithse ()
-    print("in EditWithse")
+    --print("in EditWithse")
     local se_exe = '/home/mikfer/bin/se'
     local cur_file_path = props['FilePath']
     local current_line = CurrentLine()
@@ -16,3 +16,20 @@ function EditWithse ()
     --print("current_Line: ", current_line)
     os.execute(se_exe)
 end
+
+function joinEm ()
+    local pos = editor.CurrentPos
+        
+    local line = editor:LineFromPosition(pos)
+    local sstart = editor:PositionFromLine(line)
+    local send = editor.LineEndPosition[line+1]
+    
+    editor:SetTargetRange(sstart, send)
+    editor:LinesJoin()
+    
+    editor:Clear()
+    editor:GotoPos(pos)
+    
+end
+    
+    
